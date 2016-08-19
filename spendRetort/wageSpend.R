ade3ab7039375d87fd83fb426e1d369b04f3a0d1
katerabinowitz <cathryn.rabinowitz@gmail.com>
2016-08-19 08:15:57 -0400

    update

--- spendRetort/wageSpend.R
+++ spendRetort/wageSpend.R
@@ -1,15 +1,17 @@
+setwd("/Users/katerabinowitz/Documents/DataLensDC/DC-Demographics/spendRetort")
 library(stringr)
 
 spend<-read.csv("https://raw.githubusercontent.com/TaxFoundation/data/master/real-purchasing-power/current/real-purchasing-power-2013.csv")
 spend<-spend[order(spend$rpp.in.100.dollars),]
 
-wageArea<-read.table("/Users/katerabinowitz/Documents/DataLensDC Org/oe.area.txt", sep="\t",
+#wage data via http://download.bls.gov/pub/time.series/oe/
+wageArea<-read.table("oe.area.txt", sep="\t",
                      row.names=NULL,header=T, stringsAsFactors=FALSE, strip.white=TRUE, encoding="latin1",
                      quote="",comment='',
                      colClasses=c("character","character","character","character","character"))[c(2,4)]
 colnames(wageArea)<-c("area","name")
 
-wage<-read.table("/Users/katerabinowitz/Documents/DataLensDC Org/oe.data.0.current.txt", sep="\t",
+wage<-read.table("oe.data.0.current.txt", sep="\t",
                  row.names=NULL,header=T, stringsAsFactors=FALSE, strip.white=TRUE)
 
 annWage<-subset(wage,str_sub(wage$series_id,-2,-1)=="08")
@@ -23,18 +25,26 @@ allWage<-subset(annWage,annWage$industry=="000000")
 allWage<-subset(allWage,allWage$occupation=="000000")
 
 wagePlace<-merge(allWage,wageArea,by="area")
-wagePlace$name<-gsub(" NECTA Division","",wagePlace$name)
 
-spend$name<-gsub(" \\(Nonmetropolitan Portion)","",spend$name)
+#adjust NE localities
+wagePlace$name2<-gsub(" NECTA Division","",wagePlace$name)
+wagePlace$name2<-ifelse(wagePlace$name2=="New Haven, CT","New Haven-Milford, CT",
+                    ifelse(wagePlace$name2=="Norwich-New London-Westerly, CT-RI","Norwich-New London, CT",
+                      ifelse(wagePlace$name2=="Coeur dAlene, ID","Coeur d'Alene, ID",
+                        ifelse(wagePlace$name2=="Boston-Cambridge-Newton, MA","Boston-Cambridge-Newton, MA-NH",
+                          ifelse(wagePlace$name2=="Springfield, MA-CT","Springfield, MA",
+                            ifelse(wagePlace$name2=="Manchester, NH","Manchester-Nashua, NH",
+                                   wagePlace$name2))))))
+wagePlace<-wagePlace[c(1:10,12)]
 
-wageSpend<-merge(wagePlace,spend,by="name")
-check<-subset(wagePlace,!(wagePlace$name %in% wageSpend$name))
-check2<-subset(spend,!(spend$name %in% wageSpend$name))
-checka<-subset(check,grepl("NH|MA|CT",check$name))
+spend$name2<-gsub(" \\(Nonmetropolitan Portion)","",spend$name)
+
+wageSpend<-merge(wagePlace,spend,by="name2")
 
 wageSpend$value<-as.numeric(wageSpend$value)
 wageSpend$ratio<-wageSpend$rpp.in.100.dollars/wageSpend$value
 wageSpend<-wageSpend[order(wageSpend$ratio),]
 colnames(wageSpend)
-wageSpend<-wageSpend[c(1,2,6,7,12:16)]
-write.csv(wageSpend,"/Users/katerabinowitz/Documents/DataLensDC Org/wageSpend.csv")
+wageSpend<-wageSpend[c(2,6,12:17)]
+write.csv(wageSpend,"wageSpend.csv")
+summary(wageSpend$ratio)
